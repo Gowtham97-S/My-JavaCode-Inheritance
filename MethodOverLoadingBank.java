a5ce55a7c936e658d50e85855dd4414d5db6c4ed
Gowtham <121371102+Gowtham97-S@users.noreply.github.com>
2023-01-29 20:44:59 +0530

    Update MethodOverLoadingBank.java

--- MethodOverLoadingBank.java
+++ MethodOverLoadingBank.java
@@ -3,7 +3,7 @@
 public class MethodOverLoadingBank {
 	
 	public void form(String Name,int phoneno,String gmailID ) {
-		System.out.println(    "Fill-Form:");
+		System.out.println("Fill-Form:");
 	System.out.println("1.Name - "+Name);
 	System.out.println("2.Phone No - "+phoneno);
 	System.out.println("3.Mail ID - " +gmailID);
@@ -11,15 +11,13 @@ public class MethodOverLoadingBank {
 	}
 	
 	public void form(String Name,int phoneno1,int phoneno2,String gmailID ) {
-		System.out.println(    "Fill-Form:");
-	System.out.println("1.Name - "+Name);
+		System.out.println("Fill-Form:");
+	System.out.println("1.Name - "+Name);           //methode overloaded 
 	System.out.println("2.Phone No 01 - "+phoneno1);
 	System.out.println("3.Phone No 02 - "+phoneno2);
 	System.out.println("4.Mail ID - " +gmailID);
 		
 	}
-	
-	
 	
 	public static void main (String[]args) {
 		MethodOverLoadingBank form1 = new MethodOverLoadingBank();
